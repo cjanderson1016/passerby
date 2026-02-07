d90d065ff54d488141fc950904f93983c5928dbf
cjanderson1016 <anderson.connor.j@gmail.com>
2026-02-06 21:38:59 -0600

    Add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 // import "./index.css";  // included in the Vite default template, but not currently used in the Passerby app. I am keeping it here for reference, but it is not imported in the current version of the app.
-import App from "./app/App.tsx";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app/router";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 );
